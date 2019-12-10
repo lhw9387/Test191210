81996cbfb6d6124e9af876d495643cdcb8223740
2 <2@2-PC>
2019-12-10 14:24:32 +0900

    변수 선언 및 프린트문 작성

--- Test/src/test191210/Ex01.java
+++ Test/src/test191210/Ex01.java
@@ -3,7 +3,9 @@ package test191210;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number;
+		int number1;
+		System.out.println("안녕");
 
 	}
 
